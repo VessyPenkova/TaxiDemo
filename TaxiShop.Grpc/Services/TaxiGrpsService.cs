e1a972a8f80453bab147462f075ef8d92eecfa5e
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2022-10-17 14:09:40 +0300

    Added Grps

---
+++ TaxiShop.Grpc/Services/TaxiGrpsService.cs
@@ -0,0 +1,33 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System.Runtime.CompilerServices;
+using TaxiDemo.Core.Contracts;
+
+namespace TaxiDemo.Grpc.Services
+{
+    public class TaxiGrpsService : Taxi.TaxiBase
+    {
+        private readonly ITaxiService taxiservice;
+        public TaxiGrpsService(ITaxiService _taxiservice)
+        {
+            taxiservice = _taxiservice;
+        }
+
+        public override async Task<TaxiList> GetAll(Empty request, ServerCallContext context)
+        {
+            TaxiList result = new TaxiList();
+            var taxis = await taxiservice.GetAll();
+
+            result.Items.AddRange(taxis.Select(t => new TaxiItem()
+            {
+                Name = t.Name,
+                Id = t.Id.ToString(),
+                Price = (double)t.Price,
+                Quantity = t.Quantity
+
+            }));
+
+            return result;
+        }
+    }
+}
